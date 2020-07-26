547a66bd78a55eee2e50b7c71d5e30bee20b13ab
CHO Kenichi <cho3670@gmail.com>
2020-07-26 17:34:15 +0900

    Update InfinispanClusterExec.java

--- clusterexec/src/main/java/org/infinispan/tutorial/simple/clusterexec/InfinispanClusterExec.java
+++ clusterexec/src/main/java/org/infinispan/tutorial/simple/clusterexec/InfinispanClusterExec.java
@@ -16,44 +16,47 @@ import org.infinispan.manager.DefaultCacheManager;
 
 public class InfinispanClusterExec {
 
-   public static void main(String[] args) throws Exception{
-      // Setup up a clustered cache manager
-      GlobalConfigurationBuilder global = GlobalConfigurationBuilder.defaultClusteredBuilder();
-      // Initialize the cache manager
-      DefaultCacheManager cacheManager = new DefaultCacheManager(global.build());
-      ClusterExecutor clusterExecutor = cacheManager.executor();
+    final static int MIN_SERVERS = 3;
+    final static Object obj = new Object();
 
-      //クラスタメンバが追加さえるのを待つ?
-      //Thread.sleep(1000*3);
+    public static void main(String[] args) throws Exception {
+        // Setup up a clustered cache manager
+        GlobalConfigurationBuilder global = GlobalConfigurationBuilder.defaultClusteredBuilder();
+        // Initialize the cache manager
+        DefaultCacheManager cacheManager = new DefaultCacheManager(global.build());
 
-      // コーディング量を減らすためにラムダ式で記述しています
-      CompletableFuture<Void> completableFuture = clusterExecutor.submitConsumer(cm -> {
-           // 各Data Grid ノードで実行される処理
-           int i =  new Random().nextInt(); // ランダム値を復帰値とする
-           System.out.printf( "Thread[%s] callable, value[%d]\n", Thread.currentThread().getName(), i );
-           return i;
-      }, (address, intValue, throwable) -> {
-           // 各ノードの処理結果を受け取る処理(コーディングを簡単にするためラムダ式で記述)
-           // Functionが実行されたAddress（Node）、Functionが返した値、例外が発生した場合はその原因
-           if(throwable != null) {
-              System.out.println("Address: " + address + " encountered an error: " + throwable);
-              throwable.printStackTrace();
-           }
-           System.out.printf("Address[%s], value[%s]\n", address, intValue);
-      });
-      completableFuture.whenComplete( (v, t) -> {
-          // 全部のノードの処理が終了したら行う処理
-          if (t != null) {
-            System.out.println("Exception encountered while waiting:" + t);
-            t.printStackTrace();
-          }
-          System.out.println("Distributed process compleated.");
-      });
-      
-      // Shuts down the cache manager and all associated resources
+        // クラスタメンバが一定数以上の場合分散処理を実行する
+        if (cacheManager.getMembers().size() >= NUM_SERVERS) {
+            ClusterExecutor clusterExecutor = cacheManager.executor();
+            CompletableFuture<Void> completableFuture = clusterExecutor.submitConsumer(cm -> {
+                // 各Data Grid ノードで実行される処理（コーディング量を減らすためにラムダ式で記述）
+                int i = new Random().nextInt(); // ランダム値を復帰値とする
+                System.out.printf("Tread[%s] callable, value[%d]\n", Thread.currentThread().getName(), i);
+                //obj.notify();
+                return i;
+            }, (address, intValue, throwable) -> {
+                // 各ノードの処理結果を受け取る処理(コーディングを簡単にするためラムダ式で記述)
+                // Functionが実行されたAddress（Node）、Functionが返した値、例外が発生した場合はその原因
+                if (throwable != null) {
+                    System.out.println("Address: " + address + " encountered an error: " + throwable);
+                    throwable.printStackTrace();
+                }
+                System.out.printf("Address[%s], value[%s]\n", address, intValue);
+            });
 
-Thread.sleep(1000*1000);
-      cacheManager.stop();
-   }
-
+            completableFuture.whenComplete((v, t) -> {
+                // 全部のノードの処理が終了したら行う処理
+                if (t != null) {
+                    System.out.println("Exception encountered while waiting:" + t);
+                    t.printStackTrace();
+                }
+                System.out.println("Distributed process compleated.");
+            });
+        } else {
+            System.out.println("Waiting more cluster node");
+            obj.wait();
+        }
+        // Shuts down the cache manager and all associated resources
+        cacheManager.stop();
+    }
 }
